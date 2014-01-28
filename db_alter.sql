8859f51cf2d5ecd786ffa9fd0780056158ac3e24
Andrei <andrei.badila1@gmail.com>
2014-01-28 15:43:22 +0200

    New lines on top

--- db_alter.sql
+++ db_alter.sql
@@ -1,4 +1,4 @@
+select * from appointments #newly added
 SELECT * FROM clients WHERE CoachID=7490 #new query
 SELECT * FROM users WHERE KeyID=7490
 SELECT * FROM coaches WHERE CoachID=7490
-select * from appointments #newly added
